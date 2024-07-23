a0d826dba018c656b722b5a802e4dc35530bb289
Cédric Morgantini <C22660>
2024-07-23 12:57:55 +0200

    Ajout d'une vue pour afficher les artciles. Correction des temlplates associés

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+from blog.articles.models import Article
+
+# To display the articles.
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "all_articles"
+    ordering = ["published_on"]
+    template_name = "articles/all_articles.html"
+
+    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    #     context = super().get_context_data(**kwargs)
+    #     url_website = self.request.build_absolute_uri("/order/")
+    #     context["url_website"] = url_website
+
+    #     return context
+
+
+articles_view = ArticleListView.as_view()
