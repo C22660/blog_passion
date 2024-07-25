f8bc71c78f2f31edaa60030db398e3152c5f4d12
Cédric Morgantini <C22660>
2024-07-25 10:53:59 +0200

    Ajout d'un modèle contact

--- blog/contacts/models.py
+++ blog/contacts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from django_extensions.db.fields import AutoSlugField
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+class Contact(models.Model):
+    """A model for the contact message is not mandatory. But here, we could
+    record all messages in the database for analysis purpose.
+    """
+
+    CHOICES = (
+        ('CONTACT', 'Contact'),
+        ('BUG', 'Remontée de bug'),
+    )
+
+    contact_email = models.EmailField(_("E-mail"))
+    slug = AutoSlugField(
+        "contact slug",
+        populate_from=['contact_email', 'created_at'],
+        unique=True,
+    )
+    content = models.TextField(_("message"), blank=False)
+    reason = models.CharField(max_length=15, choices = CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = _("contacts")
+
+    def __str__(self):
+        return f"{self.contact_email} ({self.created_at.day})"
