8d132511b63c90514e8899b68e0c6f09152889ec
Cédric Morgantini <C22660>
2024-07-27 18:24:58 +0200

    Installation et configuration de rest_framework_simplejwt

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.articles.urls")),
@@ -10,6 +12,8 @@ urlpatterns = [
     path("users/", include("blog.users.urls")),
     path("contacts/", include("blog.contacts.urls")),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("", include("api.articles.urls")),
 ]
 
